d7c0ceb144803b6669d27f67a4d325ed2b9a32e7
Teo Jun Jie <teojunjie@gmail.com>
2019-03-29 17:31:45 +0800

    Resolving minor bug

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 // Load environment variables
 require('dotenv').load()
-Authentication
+//Authentication
 app.use(session({
   // use the dB to store sessions @psyf
   secret: process.env.SESSION_SECRET,
