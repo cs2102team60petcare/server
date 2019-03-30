a778396fe491f7ceb77f849eece2a820843091ff
Teo Jun Jie <teojunjie@gmail.com>
2019-03-30 18:13:28 +0800

    Fixed signup pages to two separeate pages for caretaker and owner

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,3 +1,7 @@
-exports.signUp = function (req, res, next) {
-  res.render('signup', { title: 'Sign up for PetCare' })
+exports.signUpCareTaker = function (req, res, next) {
+  res.render('signupCareTaker', { title: 'Sign up for PetCare as a CareTaker' })
+}
+
+exports.signUpOwner = function (req ,res, next) {
+  res.render('signupOwner', { title : 'Sign up for Percare as an Owner' })
 }
--- routes/signup.js
+++ routes/signup.js
@@ -6,9 +6,39 @@ const queries = require('../database/queries')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
 
-router.get('/', signUpController.signUp)
+router.get('/' , function (req ,res , next) {
+  res.status(200).json({"hello" : "world"})
+})
+router.get('/caretaker', signUpController.signUpCareTaker)
 
-router.post('/', function (req, res, next) {
+router.post('/caretaker', function (req, res, next) {
+  
+  // NEED address, and phone number @kevin
+  // replace hardcode in pool.query line below to req.body.phone (numeric) and req.body.address (in json)
+
+  //DO INPUT VALIDATION @teojunjie 
+  	//valid value on all fields
+  	//use userExistsQuery. if data.rows.length != 0, REJECT
+  	//similarly, gotta check all unique fields in USERS. Please see init_petcare.sql
+
+  bcrypt.genSalt(saltRounds, function(err, salt) {
+    bcrypt.hash(req.body.password, salt, function(err, hash) {
+      pool.query(queries.signupUserInsert, [req.body.username, req.body.email, 019379123, '{}', hash], (err, res1) => {
+      	if (err) { 
+      		console.log(err) 
+      	} else {
+      		return res.status(200).json(req.body) 
+      		//res.redirect('/home')
+      	}
+      })
+    })
+  })
+})
+
+
+router.get('/owner', signUpController.signUpOwner)
+
+router.post('/owner', function (req, res, next) {
   
   // NEED address, and phone number @kevin
   // replace hardcode in pool.query line below to req.body.phone (numeric) and req.body.address (in json)
