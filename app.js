6e8ffed4b85347af13db3d6df8b446125569d27c
Teo Jun Jie <teojunjie@gmail.com>
2019-04-03 06:34:21 +0800

    Add caretaker profile

--- app.js
+++ app.js
@@ -14,18 +14,18 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 // Load environment variables
 require('dotenv').load()
-//Authentication
+// Authentication
 app.use(session({
   // use the dB to store sessions @psyf
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: false,
-  cookie: { maxAge: 180000, 
-            secure: false, 
-            httpOnly: true,   //protects against Cross Site Scripting  
-          }, 
-  name: "id"    //SECURITY NOTE: making it harder to tell we're using express-session
-  //courtesy of: https://lockmedown.com/securing-node-js-managing-sessions-express-js/
+  cookie: { maxAge: 180000,
+    secure: false,
+    httpOnly: true // protects against Cross Site Scripting
+  },
+  name: 'id' // SECURITY NOTE: making it harder to tell we're using express-session
+  // courtesy of: https://lockmedown.com/securing-node-js-managing-sessions-express-js/
 }))
 app.use(passport.initialize())
 app.use(passport.session())
@@ -42,106 +42,150 @@ var routes = require('./routes/main')
 app.use('/', routes)
 app.get('/ownerprofile', function (req, res, next) {
   res.render('ownerprofile', {
-    bids :[
-      { status : "successful",
-        bid_id : "1",
-        money : "20",
-        ending : "20/4/2019",
-        starting : "12/4/2019"
+    bids: [
+      { status: 'successful',
+        bid_id: '1',
+        money: '20',
+        ending: '20/4/2019',
+        starting: '12/4/2019'
       },
-      { status : "pending",
-      bid_id : "2",
-      money : "30",
-      ending : "20/4/2019",
-      starting : "12/4/2019"
+      { status: 'pending',
+        bid_id: '2',
+        money: '30',
+        ending: '20/4/2019',
+        starting: '12/4/2019'
 
       },
-      { status : "cancelled",
-      bid_id : "3",
-      money : "10",
-      ending : "20/4/2019",
-      starting : "12/4/2019"
+      { status: 'cancelled',
+        bid_id: '3',
+        money: '10',
+        ending: '20/4/2019',
+        starting: '12/4/2019'
 
       },
-      { status : "succesful",
-        bid_id : "4",
-        money : "50",
-        ending : "20/4/2019",
-        starting : "12/4/2019"
-      },
-    ],
-    pets : [
-      {
-        pet_id : 1,
-        name : "wuffles",
-        type : "husky",
-        biography : "easy to take care",
-        born : "20/9/16",
-        death : "Alive"
-      },
-      {
-        pet_id : 1,
-        name : "wuffles",
-        type : "husky",
-        biography : "easy to take care",
-        born : "20/9/16",
-        death : "Alive"
-      },
-      {
-        pet_id : 1,
-        name : "wuffles",
-        type : "husky",
-        biography : "easy to take care",
-        born : "20/9/16",
-        death : "Alive"
-      },
-      {
-        pet_id : 1,
-        name : "wuffles",
-        type : "husky",
-        biography : "easy to take care",
-        born : "20/9/16",
-        death : "Alive"
+      { status: 'succesful',
+        bid_id: '4',
+        money: '50',
+        ending: '20/4/2019',
+        starting: '12/4/2019'
       }
     ],
-    tasks : [
+    pets: [
       {
-        task_id : 0,
-        user_id : 3,
-        name : "teojunjie",
-        starting : "20/1/18",
-        ending : "12/5/19",
-        money : "20"
+        pet_id: 1,
+        name: 'wuffles',
+        type: 'husky',
+        biography: 'easy to take care',
+        born: '20/9/16',
+        death: 'Alive'
       },
       {
-        task_id : 0,
-        user_id : 3,
-        name : "teojunjie",
-        starting : "20/1/18",
-        ending : "12/5/19",
-        money : "20"
+        pet_id: 1,
+        name: 'wuffles',
+        type: 'husky',
+        biography: 'easy to take care',
+        born: '20/9/16',
+        death: 'Alive'
       },
       {
-        task_id : 0,
-        user_id : 3,
-        name : "teojunjie",
-        starting : "20/1/18",
-        ending : "12/5/19",
-        money : "20"
+        pet_id: 1,
+        name: 'wuffles',
+        type: 'husky',
+        biography: 'easy to take care',
+        born: '20/9/16',
+        death: 'Alive'
       },
       {
-        task_id : 0,
-        user_id : 3,
-        name : "teojunjie",
-        starting : "20/1/18",
-        ending : "12/5/19",
-        money : "20"
+        pet_id: 1,
+        name: 'wuffles',
+        type: 'husky',
+        biography: 'easy to take care',
+        born: '20/9/16',
+        death: 'Alive'
+      }
+    ],
+    tasks: [
+      {
+        task_id: 0,
+        user_id: 3,
+        name: 'teojunjie',
+        starting: '20/1/18',
+        ending: '12/5/19',
+        money: '20'
       },
+      {
+        task_id: 0,
+        user_id: 3,
+        name: 'teojunjie',
+        starting: '20/1/18',
+        ending: '12/5/19',
+        money: '20'
+      },
+      {
+        task_id: 0,
+        user_id: 3,
+        name: 'teojunjie',
+        starting: '20/1/18',
+        ending: '12/5/19',
+        money: '20'
+      },
+      {
+        task_id: 0,
+        user_id: 3,
+        name: 'teojunjie',
+        starting: '20/1/18',
+        ending: '12/5/19',
+        money: '20'
+      }
     ]
-  
+
   })
 })
 
+app.get('/caretakerprofile', function (req, res, next) {
+  res.render('caretaker', {
+    tasks: [
+      {
+        task_id: 1,
+        petname: 'mrwaffles',
+        ownername: 'jj',
+        starting: '12/3/19',
+        ending: '12/3/22'
+      }
+    ],
+    bids: [
+      {
+        bids_id: 1,
+        pet_id: 2,
+        owner_id: 3,
+        service_id: 2,
+        money: 20,
+        status: 'Successful',
+        starting: '12/3/18',
+        ending: '9/2/20'
+      }
+    ],
+    services: [
+      {
+        service_id: 1,
+        caretaker_id: 2,
+        minwage: 20,
+        status: 'Taken',
+        starting: '18/3/14',
+        ending: '16/03/18'
+      }
+    ],
+    tasksHistory: [
+      {
+        task_id: 1,
+        petname: 'mrwaffles',
+        ownername: 'jj',
+        starting: '12/3/19',
+        ending: '12/3/22'
+      }
+    ]
+  })
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
