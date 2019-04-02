da078d7dc439e55c85dba98680debba883fe884c
Teo Jun Jie <teojunjie@gmail.com>
2019-04-03 02:35:10 +0800

    Updated with owner profile page

--- app.js
+++ app.js
@@ -40,6 +40,107 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // Add routes
 var routes = require('./routes/main')
 app.use('/', routes)
+app.get('/ownerprofile', function (req, res, next) {
+  res.render('ownerprofile', {
+    bids :[
+      { status : "successful",
+        bid_id : "1",
+        money : "20",
+        ending : "20/4/2019",
+        starting : "12/4/2019"
+      },
+      { status : "pending",
+      bid_id : "2",
+      money : "30",
+      ending : "20/4/2019",
+      starting : "12/4/2019"
+
+      },
+      { status : "cancelled",
+      bid_id : "3",
+      money : "10",
+      ending : "20/4/2019",
+      starting : "12/4/2019"
+
+      },
+      { status : "succesful",
+        bid_id : "4",
+        money : "50",
+        ending : "20/4/2019",
+        starting : "12/4/2019"
+      },
+    ],
+    pets : [
+      {
+        pet_id : 1,
+        name : "wuffles",
+        type : "husky",
+        biography : "easy to take care",
+        born : "20/9/16",
+        death : "Alive"
+      },
+      {
+        pet_id : 1,
+        name : "wuffles",
+        type : "husky",
+        biography : "easy to take care",
+        born : "20/9/16",
+        death : "Alive"
+      },
+      {
+        pet_id : 1,
+        name : "wuffles",
+        type : "husky",
+        biography : "easy to take care",
+        born : "20/9/16",
+        death : "Alive"
+      },
+      {
+        pet_id : 1,
+        name : "wuffles",
+        type : "husky",
+        biography : "easy to take care",
+        born : "20/9/16",
+        death : "Alive"
+      }
+    ],
+    tasks : [
+      {
+        task_id : 0,
+        user_id : 3,
+        name : "teojunjie",
+        starting : "20/1/18",
+        ending : "12/5/19",
+        money : "20"
+      },
+      {
+        task_id : 0,
+        user_id : 3,
+        name : "teojunjie",
+        starting : "20/1/18",
+        ending : "12/5/19",
+        money : "20"
+      },
+      {
+        task_id : 0,
+        user_id : 3,
+        name : "teojunjie",
+        starting : "20/1/18",
+        ending : "12/5/19",
+        money : "20"
+      },
+      {
+        task_id : 0,
+        user_id : 3,
+        name : "teojunjie",
+        starting : "20/1/18",
+        ending : "12/5/19",
+        money : "20"
+      },
+    ]
+  
+  })
+})
 
 
 // catch 404 and forward to error handler
