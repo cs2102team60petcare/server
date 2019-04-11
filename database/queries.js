0dec1872098c5b23e420ddcb62d693df8bea113f
Psyf <gzsaif001@gmail.com>
2019-04-11 22:41:11 +0800

    Change complex3

--- database/queries.js
+++ database/queries.js
@@ -6,10 +6,8 @@ module.exports = {
   deserializeManagerQuery: 'select * from managers where email=$1;',
   isOwnerOrCaretakerQuery: 'SELECT 1 from OWNERS natural join USERS where email=$1 UNION SELECT 2 from CARETAKERS natural join USERS where email=$1;',
 
-  userExistsQuery: 'SELECT user_id FROM users WHERE email=$1;', // todo: @ jj used as check before signup
+  userExistsQuery: 'SELECT user_id FROM users WHERE email=$1;',
 
-  // TODO: @Psyf no deletion yet.
-  // call them SOFT deletes
   // WARNING: Use signUpUserInsert in a Transaction with one of the next two
   // TODO @ JJ
   signupUserInsert: 'INSERT INTO users (name, email, phone, address, password) VALUES ($1, $2, $3, $4, $5) RETURNING *',
@@ -37,7 +35,7 @@ module.exports = {
 
   getMyUpcomingTasksQuery: 'SELECT task_id, P.type, P.name as petname, U.name as ownername, U.phone, B.starting, B.ending, B.money FROM Tasks T JOIN Bids B on (T.bid_id=B.bid_id) JOIN Services S on (B.service_id=S.service_id) NATURAL JOIN Caretakers C NATURAL JOIN Pets P JOIN Users U on (B.owner_id=U.user_id) WHERE C.user_id=$1 and T.status=1 ORDER BY S.starting desc;',
   getMyTaskHistoryQuery: 'SELECT task_id, P.type, P.name as petname, U.name as ownername, B.starting, B.ending, B.money FROM Tasks T join Bids B on (T.bid_id=B.bid_id) join Services S on (B.service_id=S.service_id) JOIN Caretakers C on (S.caretaker_id=C.user_id) NATURAL JOIN Pets P JOIN Users U on (B.owner_id=U.user_id) WHERE C.user_id=$1 and T.status=2 ORDER BY S.starting desc;',
-  getMyTaskHistoryAsOwnerQuery: 'SELECT task_id, T.status, P.name as petname, U.name as caretakername, B.starting, B.ending, B.money FROM Tasks T join Bids B on (T.bid_id=B.bid_id and B.owner_id=$1) join Services S on (B.service_id=S.service_id) JOIN Caretakers C on (S.caretaker_id=C.user_id) NATURAL JOIN Pets P JOIN Users U on (S.caretaker_id=U.user_id) ORDER BY S.starting desc;', 
+  getMyTaskHistoryAsOwnerQuery: 'SELECT task_id, T.status, P.name as petname, U.name as caretakername, U.phone, B.starting, B.ending, B.money FROM Tasks T join Bids B on (T.bid_id=B.bid_id and B.owner_id=$1) join Services S on (B.service_id=S.service_id) JOIN Caretakers C on (S.caretaker_id=C.user_id) NATURAL JOIN Pets P JOIN Users U on (S.caretaker_id=U.user_id) ORDER BY S.starting desc;', 
   finishTaskUpdate: 'UPDATE Tasks SET status=2 where task_id=$1;',
   getMyReviews: 'SELECT * from REVIEWS where caretaker_id=$1 order by reviewnum desc;', 
 
@@ -60,7 +58,7 @@ module.exports = {
         'where S.status=1 and B.status=1 and S.caretaker_id=$1;',
   seeBidsForServiceQuery: 'SELECT * FROM Bids B JOIN Services S on (B.service_id=S.service_id) ' +
         'WHERE S.service_id=$1 ORDER BY B.money desc;', 
-  seeMyBidsQuery: 'SELECT * FROM Bids B JOIN Services S on (B.service_id=S.service_id) where B.owner_id=$1;',   
+  seeMyBidsQuery: 'SELECT B.bid_id, S.service_id, B.pet_id, B.money, B.status, B.starting, B.ending, U.name FROM Bids B JOIN Services S on (B.service_id=S.service_id) join Users U on (S.caretaker_id=U.user_id) where B.owner_id=$1;',   
   getAllBids: 'SELECT * FROM Bids',      
   // Triggers placingBid
   placeBidInsert: 'INSERT INTO Bids (starting, ending, money, owner_id, pet_id, service_id) VALUES ($1, $2, $3, $4, $5, $6);',
@@ -68,7 +66,6 @@ module.exports = {
   // Use when Caretaker is removing a service (can't do it if already a task)
   // NOTE: No edits. You can remove and add again if needed.
   // NOTE: triggers updateService
-  // TODO @ JJ
   removeServiceUpdate1: 'UPDATE Services SET status=0 WHERE service_id=$1;',
   removeServiceUpdate2: 'UPDATE Bids SET status=0 WHERE service_id=$1;',
 
@@ -87,18 +84,16 @@ module.exports = {
   sendReviewInsert2: 'UPDATE Tasks set status=2 where task_id=$1;', // $1 = $3 from above
   sendReviewInsert3: 'UPDATE Caretakers SET rating=((select sum(stars)::float4 from reviews where caretaker_id=$1)/(select count(*) from reviews where caretaker_id=$1)) where user_id=$1;', // $4 = $1 from above
 
-  // TODO @Psyf Trigger to ensure time
   taskCompletedupdate: 'UPDATE Tasks SET status=2 where task_id=$1;',
 
   // Use when Owners want to retract a bid (CAN do even if already a task)
   // Again, you can only remove and add bids, no edits.
   // Trigger deletingTask ensures finished tasks/bids can't be deleted (both soft and hard)
-  // TODO @ JJ
   retractBidUpdate1: 'UPDATE Bids SET status=0 WHERE bid_id=$1;',
   retractBidUpdate2: 'UPDATE Bids SET status=1 WHERE owner_id<>$1 and service_id=$2;',
   retractBidUpdate3: 'UPDATE Services SET status=1 WHERE service_id=$1;',
   retractBidUpdate4: 'DELETE FROM Tasks where bid_id=$1;',
-
+ 
   // Use when caretaker accepts a bid
   // make sure no other accepted bids present
   // Triggers updateService
@@ -119,17 +114,15 @@ module.exports = {
 
   // Complex Query 3
   // Shows the demand ratio by hour for days {1..7} (whichever we call)
-  ratioOfTaskByHourByDay: 'select extract(hour from starting)::integer as hour, ' +
+  ratioOfBidsByHourByDay: 'select extract(DOW from starting) as day, extract(hour from starting)::integer as hour, ' +
         'count(*)::float4/(select count(*) from Bids where status=2 and extract(DOW from starting)=$1) as ratio ' +
-        'from bids where status=2 and extract(DOW from starting)=$1 group by hour;',
+        'from bids where extract(DOW from starting)=$1 group by day, hour;',
 
   // Do inside a transaction
-  // TODO @ JJ
   assignRequestToMe1: 'UPDATE Requests SET status=1 where request_id=$1;',
   assignRequestToMe2: 'INSERT INTO Handles (manager_id, request_id) VALUES ($1, $2);',
 
   // Do inside a transaction
-  // TODO @ JJ
   requestSolvedUpdate1: 'UPDATE Requests SET status=2 where request_id=$1;',
   requestSolvedUpdate2: 'UPDATE Handles SET justification=$1 where request_id=$2;',
 
