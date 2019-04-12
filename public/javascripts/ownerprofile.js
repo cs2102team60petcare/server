e3a5bc84689d4257daee63753d52195266c394a3
Teo Jun Jie <teojunjie@gmail.com>
2019-04-12 08:46:08 +0800

    make graph nicer for owner profile

--- public/javascripts/ownerprofile.js
+++ public/javascripts/ownerprofile.js
@@ -54,7 +54,6 @@ $(document).ready(function () {
                       '<td>' + status + '</td>' +
                       '<tr>'
             $('.table-allbids').append(row)
-
           }
         }
       }
@@ -374,6 +373,7 @@ $(document).ready(function () {
   }
 
   var multipleData = []
+  var days = ['Monday', 'Tuesday' , 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
   var colors = ['#Ffd700', '#Ffa500', '#40e0d0', '#00ff7f', '#FFC0CB', '#00ff00', '#C6e2ff']
   for (var dataKey in xData) {
     if (xData.hasOwnProperty(dataKey)) {
@@ -382,7 +382,7 @@ $(document).ready(function () {
       var dataColumn = {
         type: 'line',
         showInLegend: true,
-        name: 'Demand by Hour',
+        name: days[dataKey - 1],
         markerType: 'square',
         color: colors[parseInt(dataKey) - 1],
         dataPoints: dataValue
